acade9cccdd4524f6eba950f6a283acfad4e02cd
Corey <corey2232@gmail.com>
2019-05-05 19:33:37 -0400

    Pull this

--- Mail_Alert/run.py
+++ Mail_Alert/run.py
@@ -3,7 +3,7 @@ from mailalert import app
 # testing for Taylor GitKraken
 # more testing
 # this works
-# tesing the pull rlsfdkjfsdksdfkjl
+# tesing the mster pull
 
 if __name__ == '__main__':
     app.run(debug=True)
